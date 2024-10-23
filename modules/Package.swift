ef8e2494797139623a0bd1eb94c4e85d53466f03
Lukas Korba <lukas.korba@seznam.cz>
2024-10-23 21:26:07 +0200

    [#1239] implement localization in Spanish
    - WIP
    
    [#1239] implement localization in Spanish
    
    - project set up

--- modules/Package.swift
+++ modules/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "modules",
+    defaultLocalization: "en",
     platforms: [
       .iOS(.v15)
     ],
@@ -85,12 +86,12 @@ let package = Package(
         .library(name: "ZecKeyboard", targets: ["ZecKeyboard"])
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.2"),
+//        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.2"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.15.0"),
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.4"),
         .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
         .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.4"),
-        .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", exact: "2.2.5"),
+        .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", exact: "2.2.6"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.27.0"),
         .package(url: "https://github.com/flexa/flexa-ios.git", from: "1.0.4"),
         .package(url: "https://github.com/pacu/zcash-swift-payment-uri", from: "0.1.0-beta.9")
