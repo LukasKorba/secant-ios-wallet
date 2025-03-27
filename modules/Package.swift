17a9a47980a0eb49f09bab03f9de0d767c0bf8ed
Lukas Korba <lukas.korba@seznam.cz>
2025-03-27 15:23:09 +0100

    Restore Wallet Coord Flow
    - Restore wallet flow redesign made as rewired navigation to a whole new flow

--- modules/Package.swift
+++ modules/Package.swift
@@ -97,10 +97,10 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0"),
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.6"),
         .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
-        .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.4"),
+        .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.5"),
         .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", from: "2.2.10"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.6.0"),
-        .package(url: "https://github.com/flexa/flexa-ios.git", from: "1.0.9"),
+        .package(url: "https://github.com/flexa/flexa-ios.git", exact: "1.0.9"),
         .package(url: "https://github.com/pacu/zcash-swift-payment-uri", from: "0.1.0-beta.10"),
         .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.1"),
         .package(url: "https://github.com/KeystoneHQ/keystone-sdk-ios/", from: "0.0.1")
@@ -511,6 +511,7 @@ let package = Package(
         .target(
             name: "OnboardingFlow",
             dependencies: [
+                "CoordFlows",
                 "Generated",
                 "ImportWallet",
                 "Models",
