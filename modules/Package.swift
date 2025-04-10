7847f69b99482024d539bd899cb23c7bfb8e65f2
Lukas Korba <lukas.korba@seznam.cz>
2025-04-10 14:12:25 +0200

    Smart Banner logic alpha version
    - I believe most of the logic is done except syncing/recovery (waiting on rust fix)

--- modules/Package.swift
+++ modules/Package.swift
@@ -41,7 +41,6 @@ let package = Package(
         .library(name: "Home", targets: ["Home"]),
         .library(name: "KeystoneHandler", targets: ["KeystoneHandler"]),
         .library(name: "LocalAuthenticationHandler", targets: ["LocalAuthenticationHandler"]),
-        .library(name: "LocalNotification", targets: ["LocalNotification"]),
         .library(name: "LogsHandler", targets: ["LogsHandler"]),
         .library(name: "MnemonicClient", targets: ["MnemonicClient"]),
         .library(name: "Models", targets: ["Models"]),
@@ -102,6 +101,7 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
         .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.5"),
 //        .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", from: "2.2.10"),
+//        .package(url: "https://github.com/LukasKorba/ZcashLightClientKit", revision: "870f092bf3f85a26772d1bc37b060f6b6b1c1698"),
         .package(url: "https://github.com/LukasKorba/ZcashLightClientKit", branch: "1537-Birthday-estimate-based-on-a-date"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.6.0"),
         .package(url: "https://github.com/flexa/flexa-ios.git", exact: "1.0.9"),
@@ -459,14 +459,6 @@ let package = Package(
             path: "Sources/Dependencies/LocalAuthenticationHandler"
         ),
         .target(
-            name: "LocalNotification",
-            dependencies: [
-                "Generated",
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-            ],
-            path: "Sources/Dependencies/LocalNotification"
-        ),
-        .target(
             name: "LogsHandler",
             dependencies: [
                 "Utils",
@@ -688,7 +680,6 @@ let package = Package(
                 "Generated",
                 "Home",
                 "LocalAuthenticationHandler",
-                "LocalNotification",
                 "MnemonicClient",
                 "Models",
                 "NotEnoughFreeSpace",
@@ -893,7 +884,6 @@ let package = Package(
             name: "SmartBanner",
             dependencies: [
                 "Generated",
-                "LocalNotification",
                 "Models",
                 "NetworkMonitor",
                 "SDKSynchronizer",
--- modules/Sources/Features/Root/RootInitialization.swift
+++ modules/Sources/Features/Root/RootInitialization.swift
@@ -501,7 +501,6 @@ extension Root {
                 }
                 state.splashAppeared = true
                 state.isRestoringWallet = false
-                localNotification.clearNotifications()
                 userDefaults.remove(Constants.udIsRestoringWallet)
                 userDefaults.remove(Constants.udLeavesScreenOpen)
                 flexaHandler.signOut()
