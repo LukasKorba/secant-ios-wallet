52bc0574ae0c621b3ddfcff838f5142f700ffbd2
Lukas Korba <lukas.korba@seznam.cz>
2025-10-06 10:21:24 +0200

    xcode26 build
    - buildable solution with some errors to FIXME

--- modules/Sources/Features/TransactionDetails/TransactionDetailsStore.swift
+++ modules/Sources/Features/TransactionDetails/TransactionDetailsStore.swift
@@ -419,7 +419,7 @@ public struct TransactionDetails {
                 if let account = state.selectedWalletAccount?.account, let umSwapId = state.umSwapId, needsUpdate {
                     userMetadataProvider.update(umSwapId)
                     try? userMetadataProvider.store(account)
-                    _ = state.transaction.checkAndUpdateWith(umSwapId)
+                    state.transaction.checkAndUpdateWith(umSwapId)
                     state.$transactions.withLock { $0[id: state.transaction.id] = state.transaction }
                     return .none
                 }
